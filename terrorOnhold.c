de01661ab4293c616b98499034d92682092f3cab
Niranjana <niranjana.arunmenon@gmail.com>
2022-09-06 12:44:36 +1000

    terror that is style guide

---
+++ terrorOnhold.c
@@ -0,0 +1,13 @@
+// COMP 2521 beginning
+//
+// Pride on hold
+// Niranjana (z5417727@unsw.edu.au)
+//
+// Written: 6/09/22
+
+#include <stdio.h>
+
+int main(void) {
+  printf("This is . going to be bad. this is how the style guide is meant to be.\n");
+  return 0;
+}
