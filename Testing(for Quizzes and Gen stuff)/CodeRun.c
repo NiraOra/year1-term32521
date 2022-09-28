3d553837c26dff64ef77bebd9c1e1395b7c85739
Niranjana <niranjana.arunmenon@gmail.com>
2022-09-28 12:07:37 +1000

    added testing place: test folder rather (need to learn how to do a lot of things

--- Testing(for Quizzes and Gen stuff)/CodeRun.c
+++ Testing(for Quizzes and Gen stuff)/CodeRun.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <CodeRun.h>
+#include "CodeRun.h"
 
-struct node {
-  int data;
-  struct node *next;
-};
+
+#define QUIT 'q'
+#define QUIZ_QUESTION 'q_q'
 
 int main(int argc, char *argv[]) {
-  
   //Testing quiz 2 question 6
+  //assert(argc > 2);
+  showCommand();
+  /*if (argv[1] == QUIT) {
+    exit(1);
+  }*/
   struct node *new = malloc(sizeof(*new));
   struct node *newTwo = malloc(sizeof(*newTwo));
   struct node *newThree = malloc(sizeof(*newThree));
@@ -26,24 +29,27 @@ int main(int argc, char *argv[]) {
   //newFour->next = NULL;
   //newFive->next = NULL;
   
-  struct node *head = fn(new);
+  // struct node *head = fn(new);
   
-
   //struct node *new = NULL;
   //struct node *newTwo = create_node(3);
   //new->next = newTwo;
-  //new = append(new, 7);
- 
-  for (struct node *current = new; current != NULL; current = current->next) {
-    printf("%d ", current->data);
-  }
-  printf("\n");
+  new = append(new, 7);
+  print(new);
+  
 
+}
+
+void showCommand() {
+  printf("Choose From the following:\n");
+  printf("1. Quit: q\n");
+  printf("2. Quiz Question: q_q\n");
+  printf("3. Print: p\n");
 }
 
 // reveres list; list2 is like a new head (2-> last node of the first), and the list2 is updated accordingly
 // quiz2 q6
-/*struct node *fn(struct node *head) {
+struct node *fn(struct node *head) {
   if (head == NULL) {
     return NULL;
   }
@@ -55,15 +61,21 @@ int main(int argc, char *argv[]) {
   head->next = NULL;
   return list2;
 
-}*/
+}
 
 // quiz 2 question 4
 struct node *append(struct node *list, int value) {
-  if (list == NULL) {
-    return create_node(value);
+  if (list->next == NULL) {
+    // in the quiz
+    //return create_node(value);
+    
+    //ideal solution:
+    struct node *temp = create_node(value);
+    return list->next = temp;
+    
   } else {
     append(list->next, value);
-    //return list;
+    return list;
   }
 }
 
@@ -74,5 +86,12 @@ struct node *create_node(int value) {
   return new;
 }
 
+// print the values of the linked list
+void print(struct node *head) {
+  for (struct node *current = head; current != NULL; current = current->next) {
+    printf("%d ", current->data);
+  }
+  printf("\n");
+}
 
 
--- Testing(for Quizzes and Gen stuff)/CodeRun.h
+++ Testing(for Quizzes and Gen stuff)/CodeRun.h
@@ -1,3 +1,24 @@
+#ifndef CODERUN_H
+#define CODERUN_H
+
+struct node {
+  int data;
+  struct node *next;
+};
+
+// week 2 quiz question 6: reverses list
 struct node *fn(struct node *head);
+
+// week 2 quiz question 4: adds to end of list (doesn't work for values more than NULL)
 struct node *append(struct node *head, int value);
+
+// week 2 quiz question 4: creates a new node
 struct node *create_node(int value);
+
+// general helper functions
+// 1.
+void print(struct node *head);
+// 2.
+void showCommand();
+
+#endif
